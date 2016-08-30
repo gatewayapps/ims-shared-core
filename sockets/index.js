cd9555c71336f1e009b04678f1edcbb1cee58138
Daniel Gary <danielgary@live.com>
2016-08-30 13:19:50 -0500

    implemented sockets

---
+++ sockets/index.js
@@ -0,0 +1,87 @@
+const io = require('socket.io-client');
+const jwt = require('jsonwebtoken');
+const Promise = require('bluebird');
+
+class HubSocket {
+    constructor(config) {
+
+    }
+}
+var socket, context, pendingRequests;
+pendingRequests = [];
+
+
+
+socket.on('connect', (ev) => {
+    console.log("Connected!");
+});
+socket.on('an event', (ev) => {
+    console.log(ev);
+});
+socket.on('event', (payload) => {
+    console.log(payload);
+});
+
+socket.emit('event', { your: 'mom' });
+
+module.exports = {
+    use: function (config) {
+        context = config;
+        socket = io(context.hubUrl);
+
+        socket.on('connect', (ev) => {
+            console.log('Connected to ' + context.hubUrl);
+            this.emit('register', { package: context.packageId });
+        });
+
+        socket.on('response', (contents) => {
+            for (var i = 0; i < pendingRequests.length; i++) {
+                if (pendingRequests[i].__id === contents.id) {
+                    pendingRequests[i].resolve(contents);
+                    pendingRequests.splice(i, 1);
+                    return;
+                }
+            }
+        });
+        socket.on('error', (contents) => {
+            for (var i = 0; i < pendingRequests.length; i++) {
+                if (pendingRequests[i].__id === contents.id) {
+                    pendingRequests[i].reject(contents);
+                    pendingRequests.splice(i, 1);
+                    return;
+                }
+            }
+        });
+
+    },
+
+    //THIS METHOD HAS NOT BEEN TESTED TO WORK CORRECTLY
+    requestAsync: function (to, payload) {
+        var req = {
+            id: pendingRequests.length + 1,
+            to: to,
+            package: context.packageId,
+            contents: payload
+        };
+
+        var promise = new Promise();
+        promise.__id = req.id;
+        pendingRequests.push(promise);
+        this.emit("request", req);
+        return promise;
+
+    },
+    emit: function (type, payload) {
+
+        //Wraps the emit function to sign the contents.
+        //The server only retransmits signed payloads.
+        //This should prevent spam in the socket.
+        payload = jwt.encode(payload, context.secret);
+        socket.emit(type, { contents: payload, package: context.packageId });
+    },
+    on: function (type, callback) {
+        socket.on(type, callback);
+    }
+};
+
+
