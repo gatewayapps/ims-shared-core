5e2fc114809f2ece4f5d921cb0fd5c505cb97d3b
Daniel Gary <danielgary@live.com>
2017-01-18 14:35:53 -0600

    fixing package.json

--- src/index.js
+++ src/index.js
@@ -5,15 +5,10 @@ const phoneNumberUtils = require('./lib/phoneNumberUtils');
 const PermissionHandler = require('./lib/PermissionHandler');
 const TreeHelper = require('./lib/TreeHelper');
 
-import {createLogger} from './lib/Logger'
-import Logger from './lib/Logger'
-
 
 module.exports = {
     Constants: constants,
     PermissionHandler: PermissionHandler,
     PhoneNumberUtils: phoneNumberUtils,
-    TreeHelper: TreeHelper,
-    logger: Logger,
-    createLogger: createLogger
+    TreeHelper: TreeHelper
 };
