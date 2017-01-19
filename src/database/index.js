7f5542c72a647e8ab37b5e2a323286a9b9c51f4b
Daniel Gary <danielgary@live.com>
2017-01-19 10:57:07 -0600

    fixed issue with db init

--- src/database/index.js
+++ src/database/index.js
@@ -3,47 +3,45 @@ const Sequelize = require('sequelize');
 var instances = {};
 
 module.exports = (config, databaseNameOverride) => {
-    var dbName = databaseNameOverride || config.databaseName;
-    if (!instances[dbName]) {
-        var logging = (
-          config.logging ? config.logging : (
-            process.env.NODE_ENV === 'development' ? console.log : false
-          )
-        )
-        if(logging){
-          console.log(`Connecting to ${dbName}`)
-        }
-        instances[dbName] = new Sequelize(dbName,
-            config.username,
-            config.password, {
-                host: config.server,
-                dialect: 'mssql',
-                dialectOptions: {
-                    instanceName: config.instanceName,
-                    requestTimeout: 300000
-                },
-                pool: {
-                    max: 5,
-                    min: 0,
-                    idle: 10000
-                },
-                options: {
-                    retry: {
-                        max: 3
-                    }
-                },
-                define: {
-                    timestamps: false
-                },
-                logging: logging
-            });
-        instances[dbName].authenticate().complete(function(err){
-          if(err){
-            console.error(err)
-          } else {
-            console.log(`Connected to ${dbName}`)
+  var dbName = databaseNameOverride || config.databaseName;
+  if (!instances[dbName]) {
+    var logging = (
+      config.logging ? config.logging : (
+        process.env.NODE_ENV === 'development' ? console.log : false
+      )
+    )
+    if (logging) {
+      console.log(`Connecting to ${dbName}`)
+    }
+    instances[dbName] = new Sequelize(dbName,
+      config.username,
+      config.password, {
+        host: config.server,
+        dialect: 'mssql',
+        dialectOptions: {
+          instanceName: config.instanceName,
+          requestTimeout: 300000
+        },
+        pool: {
+          max: 5,
+          min: 0,
+          idle: 10000
+        },
+        options: {
+          retry: {
+            max: 3
           }
-        })
+        },
+        define: {
+          timestamps: false
+        },
+        logging: logging
+      });
+    instances[dbName].authenticate().then(() => {
+        console.log(`${dbName} connected`)
+      }).catch((err)=>{
+        console.error(err)
+      })
     }
     return instances[dbName];
 }
