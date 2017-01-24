33a3c1e5820a9f1cf27f3e624e6f76de1eae2276
Daniel Gary <danielgary@live.com>
2017-01-24 15:18:11 -0600

    removed phoneNumberUtils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const constants = require('./lib/constants');
-const phoneNumberUtils = require('./lib/phoneNumberUtils');
+
 const PermissionHandler = require('./lib/PermissionHandler');
 const TreeHelper = require('./lib/TreeHelper');
 
@@ -9,6 +9,5 @@ const TreeHelper = require('./lib/TreeHelper');
 module.exports = {
     Constants: constants,
     PermissionHandler: PermissionHandler,
-    PhoneNumberUtils: phoneNumberUtils,
     TreeHelper: TreeHelper
 };
