512c6be6de74febfef8b88696ace4b601792d5d9
Justin Neff <justin@gatewayapps.com>
2017-05-23 11:27:14 -0500

    closes #16 - ImageCache handler

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,8 @@
+import ImageCache from './ImageCache'
+
 module.exports = {
   Authentication: require('./Authentication'),
+  ImageCache: ImageCache,
   FileUpload: require('./FileUpload'),
   CheckPermission: require('./Permissions')
 }
