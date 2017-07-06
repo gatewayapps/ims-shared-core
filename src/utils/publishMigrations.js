e16312d122f52146570f7f3ea8f692a266efc747
Daniel Gary <danielgary@live.com>
2017-07-06 12:41:34 -0500

    closes #25 adds uploadMigrationFile to utils

---
+++ src/utils/publishMigrations.js
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import request from 'request'
+const Constants = require('../lib/constants')
+const PACKAGE_MIGRATION_ENDPOINT = '/api/package/migrations'
+
+export default function uploadMigrationFile (migrationFile, imsPackage, replacements) {
+  return new Promise((resolve, reject) => {
+    try {
+      const fileStream = fs.createReadStream(migrationFile)
+      const formData = {
+        file: {
+          value: fileStream,
+          options: {
+            filename: 'migration.zip'
+          }
+        },
+        replacements: JSON.stringify(replacements)
+      }
+
+      const url = `${imsPackage.hubUrl}${PACKAGE_MIGRATION_ENDPOINT}`.replace('//', '/').replace('http:/', 'http://')
+      const headers = {}
+      headers[Constants.RequestHeaders.PackageId] = imsPackage.packageId
+      headers[Constants.RequestHeaders.PackageSecret] = imsPackage.secret
+      request.post({ url: url, formData: formData, headers: headers }, (err, response, body) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(JSON.parse(body))
+        }
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
