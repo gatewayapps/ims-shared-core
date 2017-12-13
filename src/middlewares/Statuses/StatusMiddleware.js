cd6c9c958bccc46ae3dfe22f33667b7f4b0e6fa3
BrandonMcNew <BrandonMcNew@DESKTOP-3RL8VKE>
2017-12-13 15:53:12 -0600

    updates

--- src/middlewares/Statuses/StatusMiddleware.js
+++ src/middlewares/Statuses/StatusMiddleware.js
@@ -1,16 +1,6 @@
-import getContext from './DataContext'
 import du from 'du'
 
-export default function StatusMiddleware (app, config, activitiesPath, mongoConnectionString) {
-  // const auth = createAuthenticationMiddleware(config)
-  const dbo = getContext(mongoConnectionString)
-  dbo.runCommand({ ping: 1 }, function (err, res) {
-    if (err) {
-      console.error(err)
-    }
-    if (!err && res.ok) console.log('connected to mongo!')
-  })
-
+export default function StatusMiddleware (app, config) {
   app.use('/api/core/status', (req, res, next) => {
     du(config.FILE_STORAGE_PATH, (err, sizeInBytes) => {
       if (err) {
