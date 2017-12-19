f9bb52d184b588712968b62c1de3983e3e7353b4
Daniel Gary <daniel@gatewayapps.com>
2017-12-19 09:10:56 -0600

    export StatusMiddleware for hub consumption

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,12 @@
 import ImageCache from './ImageCache'
 import Activity from './Activities/Activity'
 import ActivityMiddleware from './Activities'
-
+import StatusMiddleware from './Statuses/StatusMiddleware'
 module.exports = {
   Authentication: require('./Authentication'),
   Activity: Activity,
   ActivityMiddleware: ActivityMiddleware,
+  StatusMiddleware: StatusMiddleware,
   ImageCache: ImageCache,
   FileUpload: require('./FileUpload'),
   CheckPermission: require('./Permissions')
