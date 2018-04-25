771f6b5b90333d36c796b140a893e1657263b3f5
Daniel Gary <daniel@gatewayapps.com>
2018-04-25 14:17:01 -0500

    expose contract middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,10 +1,12 @@
 import ImageCache from './ImageCache'
 import Activity from './Activities/Activity'
+import ContractsMiddleware from './ContractMiddleware'
 import ActivityMiddleware from './Activities'
 module.exports = {
   Authentication: require('./Authentication'),
   Activity: Activity,
   ActivityMiddleware: ActivityMiddleware,
+  ContractsMiddleware: ContractsMiddleware,
   ImageCache: ImageCache,
   FileUpload: require('./FileUpload'),
   CheckPermission: require('./Permissions')
