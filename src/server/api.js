8108dafca12faf98f76a059a68f94f0ad00c728f
Daniel Gary <daniel@gatewayapps.com>
2019-04-19 10:15:26 -0500

    swagger is now optional

--- src/server/api.js
+++ src/server/api.js
@@ -1,16 +1,16 @@
-import path from 'path'
-import fs from 'fs'
-import express from 'express'
-import SwaggerExpress from 'swagger-express-mw'
-import logger from '../logger'
-import constants from '../lib/constants'
-import createAuthenticationMiddleware from '../middlewares/Authentication'
-import FileUploadMiddleware from '../middlewares/FileUpload'
-import PermissionCheckMiddleware from '../middlewares/Permissions'
-import ContractMiddleware from '../middlewares/ContractMiddleware'
-import { ActivityMiddleware } from '../middlewares/Activities'
-import createBadgeMiddleware from '../middlewares/BadgeSignatureVerification'
-import ImageCache from '../middlewares/ImageCache'
+import path from "path";
+import fs from "fs";
+import express from "express";
+import SwaggerExpress from "swagger-express-mw";
+import logger from "../logger";
+import constants from "../lib/constants";
+import createAuthenticationMiddleware from "../middlewares/Authentication";
+import FileUploadMiddleware from "../middlewares/FileUpload";
+import PermissionCheckMiddleware from "../middlewares/Permissions";
+import ContractMiddleware from "../middlewares/ContractMiddleware";
+import { ActivityMiddleware } from "../middlewares/Activities";
+import createBadgeMiddleware from "../middlewares/BadgeSignatureVerification";
+import ImageCache from "../middlewares/ImageCache";
 
 /* requestHandlers structure
   - onFileUploadRequest : (req,res, fileDetails)
@@ -20,160 +20,218 @@ import ImageCache from '../middlewares/ImageCache'
 */
 
 export default class Api {
-  constructor (serverConfig, swaggerConfig, middlewares, requestHandlers, onException, contractsDirectory = './api/contracts', activitiesDirectory = './api/activities') {
-    this.onException = onException
-    this.requestHandlers = requestHandlers
-    this.serverConfig = serverConfig
-    this.middlewares = middlewares
-    this.contractsDirectory = contractsDirectory
-    this.activitiesDirectory = activitiesDirectory
+  constructor(
+    serverConfig,
+    swaggerConfig,
+    middlewares,
+    requestHandlers,
+    onException,
+    contractsDirectory = "./api/contracts",
+    activitiesDirectory = "./api/activities"
+  ) {
+    this.onException = onException;
+    this.requestHandlers = requestHandlers;
+    this.serverConfig = serverConfig;
+    this.middlewares = middlewares;
+    this.contractsDirectory = contractsDirectory;
+    this.activitiesDirectory = activitiesDirectory;
 
     try {
-      this.verifySwaggerConfig(swaggerConfig)
-
-      this.authenticationMiddleware = createAuthenticationMiddleware(serverConfig).swaggerMiddleware
-
-      swaggerConfig.securityHandlers = {
-        token: this.defaultSecurityHandler.bind(this),
-        package: this.defaultSecurityHandler.bind(this)
+      if (swaggerConfig) {
+        this.verifySwaggerConfig(swaggerConfig);
+        this.authenticationMiddleware = createAuthenticationMiddleware(
+          serverConfig
+        ).swaggerMiddleware;
+        swaggerConfig.securityHandlers = {
+          token: this.defaultSecurityHandler.bind(this),
+          package: this.defaultSecurityHandler.bind(this)
+        };
+        this.swaggerConfig = swaggerConfig;
       }
-
-      this.swaggerConfig = swaggerConfig
-
-      this.handleError = this.handleError.bind(this)
-      this.createApp = this.createApp.bind(this)
+      this.handleError = this.handleError.bind(this);
+      this.createApp = this.createApp.bind(this);
     } catch (err) {
-      this.handleError(err)
-    }
+      this.handleError(err);
+    } 
   }
 
-  defaultSecurityHandler (req, authOrSecDef, scopesOrApiKey, next) {
-    return this.authenticationMiddleware(req, authOrSecDef, scopesOrApiKey, (error) => {
-      logger.api(req)
-      if (error) {
-        logger.error({ err: error })
+  defaultSecurityHandler(req, authOrSecDef, scopesOrApiKey, next) {
+    return this.authenticationMiddleware(
+      req,
+      authOrSecDef,
+      scopesOrApiKey,
+      error => {
+        logger.api(req);
+        if (error) {
+          logger.error({ err: error });
+        }
+        next(error);
       }
-      next(error)
-    })
+    );
   }
 
-  handleError (err) {
+  handleError(err) {
     if (err) {
       if (this.onException) {
-        this.onException(err)
+        this.onException(err);
       }
-      logger.error(err)
+      logger.error(err);
       if (err.validationErrors) {
-        logger.error(err.validationErrors)
+        logger.error(err.validationErrors);
       }
     }
   }
 
-  createApp (cb) {
-    SwaggerExpress.create(this.swaggerConfig, (err, swaggerExpress) => {
-      if (err) {
-        this.handleError(err)
-        return cb(err)
-      }
+  createApp(cb) {
+    if (this.swaggerConfig) {
+      SwaggerExpress.create(this.swaggerConfig, (err, swaggerExpress) => {
+        if (err) {
+          this.handleError(err);
+          return cb(err);
+        }
 
-      const app = express()
-      this.connectMiddlewares(app, this.middlewares, this.contractsDirectory, this.activitiesDirectory)
+        const app = express();
+        this.connectMiddlewares(
+          app,
+          this.middlewares,
+          this.contractsDirectory,
+          this.activitiesDirectory
+        );
 
-      swaggerExpress.register(app)
-      cb(null, app)
-    })
+        swaggerExpress.register(app);
+        cb(null, app);
+      });
+    } else {
+      const app = express();
+      this.connectMiddlewares(
+        app,
+        this.middlewares,
+        this.contractsDirectory,
+        this.activitiesDirectory
+      );
+      cb(null, app);
+    }
   }
 
-  connectMiddlewares (app, middlewares, contractsDirectory, activitiesDirectory) {
+  connectMiddlewares(
+    app,
+    middlewares,
+    contractsDirectory,
+    activitiesDirectory
+  ) {
     // If middlewares are provided, use them here
     if (middlewares && Array.isArray(middlewares)) {
       for (var i = 0; i < middlewares.length; i++) {
-        if (middlewares[i].route && middlewares[i].handler && middlewares[i].method) {
-          const method = middlewares[i].method
-          if (method === 'GET') {
-            app.get(middlewares[i].route, middlewares[i].handler)
-          } else if (method === 'POST') {
-            app.post(middlewares[i].route, middlewares[i].handler)
-          } else if (method === 'DELETE') {
-            app.delete(middlewares[i].route, middlewares[i].handler)
-          } else if (method === 'PUT') {
-            app.put(middlewares[i].route, middlewares[i].handler)
+        if (
+          middlewares[i].route &&
+          middlewares[i].handler &&
+          middlewares[i].method
+        ) {
+          const method = middlewares[i].method;
+          if (method === "GET") {
+            app.get(middlewares[i].route, middlewares[i].handler);
+          } else if (method === "POST") {
+            app.post(middlewares[i].route, middlewares[i].handler);
+          } else if (method === "DELETE") {
+            app.delete(middlewares[i].route, middlewares[i].handler);
+          } else if (method === "PUT") {
+            app.put(middlewares[i].route, middlewares[i].handler);
           }
-        } else if (typeof middlewares[i] === 'function') {
-          app.use(middlewares[i])
+        } else if (typeof middlewares[i] === "function") {
+          app.use(middlewares[i]);
         }
       }
     }
 
     // Connect file upload and download middlewares
-    FileUploadMiddleware(app, this.serverConfig, { uploadCallback: this.requestHandlers.onFileUploadRequest })
-    app.get('/api/download/:id', this.requestHandlers.onFileDownloadRequest)
-    app.get(constants.GlobalUrls.BadgeUrl, createBadgeMiddleware(this.serverConfig), this.requestHandlers.onBadgesRequest)
+    FileUploadMiddleware(app, this.serverConfig, {
+      uploadCallback: this.requestHandlers.onFileUploadRequest
+    });
+    app.get("/api/download/:id", this.requestHandlers.onFileDownloadRequest);
+    app.get(
+      constants.GlobalUrls.BadgeUrl,
+      createBadgeMiddleware(this.serverConfig),
+      this.requestHandlers.onBadgesRequest
+    );
 
-    app.get('/api/images/:id', ImageCache({
-      cacheDir: this.serverConfig.cachePath,
-      debugLogger: logger.debug,
-      errorLogger: logger.error,
-      getRawStream: this.requestHandlers.onGetRawStreamRequest // attachmentService.getStreamAsync
-    }))
+    app.get(
+      "/api/images/:id",
+      ImageCache({
+        cacheDir: this.serverConfig.cachePath,
+        debugLogger: logger.debug,
+        errorLogger: logger.error,
+        getRawStream: this.requestHandlers.onGetRawStreamRequest // attachmentService.getStreamAsync
+      })
+    );
 
     // Send the swagger file if it's requested
-    app.get('/swagger', (req, res) => {
-      res.sendFile(this.swaggerConfig.swaggerFile)
-    })
+    app.get("/swagger", (req, res) => {
+      res.sendFile(this.swaggerConfig.swaggerFile);
+    });
 
-    app.get('/api/*', (req, res, next) => {
-        // Force client not to cache api get requests.
-        // This is mainly for Internet Explorer.
+    app.get("/api/*", (req, res, next) => {
+      // Force client not to cache api get requests.
+      // This is mainly for Internet Explorer.
       res.set({
-        'Cache-Control': 'private, max-age=0, no-cache',
-        'Pragma': 'no-cache',
-        'Expires': 0
-      })
-      next()
-    })
-    PermissionCheckMiddleware(app, this.serverConfig)
+        "Cache-Control": "private, max-age=0, no-cache",
+        Pragma: "no-cache",
+        Expires: 0
+      });
+      next();
+    });
+    PermissionCheckMiddleware(app, this.serverConfig);
 
-    contractsDirectory = getAbsolutePath(contractsDirectory, this.serverConfig.serverRoot)
-    activitiesDirectory = getAbsolutePath(activitiesDirectory, this.serverConfig.serverRoot)
+    contractsDirectory = getAbsolutePath(
+      contractsDirectory,
+      this.serverConfig.serverRoot
+    );
+    activitiesDirectory = getAbsolutePath(
+      activitiesDirectory,
+      this.serverConfig.serverRoot
+    );
 
     if (contractsDirectory && fs.existsSync(contractsDirectory)) {
-      ContractMiddleware(app, this.serverConfig, contractsDirectory)
+      ContractMiddleware(app, this.serverConfig, contractsDirectory);
     }
 
     if (activitiesDirectory && fs.existsSync(activitiesDirectory)) {
-      ActivityMiddleware(app, this.serverConfig, activitiesDirectory, this.serverConfig.mongoConnectionString)
+      ActivityMiddleware(
+        app,
+        this.serverConfig,
+        activitiesDirectory,
+        this.serverConfig.mongoConnectionString
+      );
     }
   }
 
-  verifySwaggerConfig (swaggerConfig) {
+  verifySwaggerConfig(swaggerConfig) {
     if (!swaggerConfig) {
-      throw new Error('Api requires a swagger config object')
+      throw new Error("Api requires a swagger config object");
     }
     if (!swaggerConfig.appRoot) {
-      throw new Error('swaggerConfig.appRoot cannot be undefined or empty')
+      throw new Error("swaggerConfig.appRoot cannot be undefined or empty");
     }
     if (!swaggerConfig.configDir) {
-      throw new Error('swaggerConfig.configDir cannot be undefined or empty')
+      throw new Error("swaggerConfig.configDir cannot be undefined or empty");
     }
     if (!swaggerConfig.swaggerFile) {
-      throw new Error('swaggerConfig.swaggerFile cannot be undefined or empty')
+      throw new Error("swaggerConfig.swaggerFile cannot be undefined or empty");
     }
   }
 }
 
-function getAbsolutePath (dir, serverRoot) {
+function getAbsolutePath(dir, serverRoot) {
   if (!path.isAbsolute(dir)) {
     if (!serverRoot) {
-      return undefined
+      return undefined;
     } else {
-      const result = path.resolve(path.join(serverRoot, dir))
+      const result = path.resolve(path.join(serverRoot, dir));
       if (path.isAbsolute(result)) {
-        return result
+        return result;
       } else {
-        return undefined
+        return undefined;
       }
     }
   }
-  return dir
+  return dir;
 }
